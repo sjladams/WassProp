870826d071213df68adb206ee60cf40488cb1948
sjladams <steven.adams.95@hotmail.com>
2024-11-01 11:26:24 +0100

    - rename _locs to _locs_inner for consistency with discretize_distributions package - split up concatenation in get_upper and get_lower for clarity

--- regions.py
+++ regions.py
@@ -2,13 +2,13 @@ import torch
 
 
 class HyperRectangularVoronoiPartition:
-    def __init__(self, locs: torch.Tensor, loc_shell: torch.Tensor, shell: torch.Tensor):
-        if not self._are_locs_in_grid(locs):
+    def __init__(self, locs_inner: torch.Tensor, loc_shell: torch.Tensor, shell: torch.Tensor):
+        if not self._are_locs_in_grid(locs_inner):
             raise ValueError("locs must be in a grid")
         self._loc_shell = loc_shell
         self._shell = shell
-        self._locs = locs
-        self._num_dims = locs.size(-1)
+        self._locs_inner = locs_inner
+        self._num_dims = locs_inner.size(-1)
         self._lower = self._get_lower()
         self._upper = self._get_upper()
 
@@ -19,7 +19,7 @@ class HyperRectangularVoronoiPartition:
 
     @property
     def locs(self):
-        return torch.cat((self._locs, self._loc_shell.unsqueeze(-2)), dim=-2)
+        return torch.cat((self._locs_inner, self._loc_shell.unsqueeze(-2)), dim=-2)
 
     # @locs.setter
     # def locs(self, locs: torch.Tensor):
@@ -37,26 +37,32 @@ class HyperRectangularVoronoiPartition:
     def upper(self):
         return self._upper
 
+    @property
+    def shell(self):
+        return self._shell
+
     @staticmethod
     def _are_locs_in_grid(locs: torch.Tensor):
         unique_elements_per_n = torch.tensor([torch.unique(locs[:, i]).numel() for i in range(locs.size(1))])
         return unique_elements_per_n.prod() == torch.unique(locs,dim=0).size(0)
 
     def _get_upper(self):
-        pos_diff = (self._locs.unsqueeze(-3) - self._locs.unsqueeze(-2)).clip(0, torch.inf)
+        pos_diff = (self._locs_inner.unsqueeze(-3) - self._locs_inner.unsqueeze(-2)).clip(0, torch.inf)
         mask = pos_diff == 0.
         pos_diff[mask] = self._shell[..., 1].unsqueeze(0).expand(pos_diff.shape)[mask]
-        return torch.cat((self._locs + 0.5 * pos_diff.min(dim=-2).values,
-                          torch.zeros(1, self._num_dims).fill_(torch.inf)),
-                         dim=-2)
+
+        upper_inner = self._locs_inner + 0.5 * pos_diff.min(dim=-2).values
+        upper_shell = torch.zeros(self._num_dims).fill_(torch.inf)
+        return torch.cat((upper_inner, upper_shell.unsqueeze(-2)), dim=-2)
 
     def _get_lower(self):
-        neg_diff = (self._locs.unsqueeze(-3) - self._locs.unsqueeze(-2)).clip(-torch.inf, 0)
+        neg_diff = (self._locs_inner.unsqueeze(-3) - self._locs_inner.unsqueeze(-2)).clip(-torch.inf, 0)
         mask = neg_diff == 0.
         neg_diff[mask] = self._shell[..., 0].unsqueeze(0).expand(neg_diff.shape)[mask]
-        return torch.cat((self._locs + 0.5 * neg_diff.max(dim=-2).values,
-                          torch.zeros(1, self._num_dims).fill_(-torch.inf)),
-                         dim=-2)
+
+        lower_inner = self._locs_inner + 0.5 * neg_diff.max(dim=-2).values
+        lower_shell = torch.zeros(self._num_dims).fill_(-torch.inf)
+        return torch.cat((lower_inner, lower_shell.unsqueeze(-2)), dim=-2)
 
     @property
     def center(self):
