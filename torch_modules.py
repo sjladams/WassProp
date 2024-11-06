f7ae9a475f98b8f77426bbbb6a8fb9b9e4d25eed
sjladams <steven.adams.95@hotmail.com>
2024-11-06 23:02:45 +0100

    rename modules + add todos

--- torch_modules.py
+++ torch_modules.py
@@ -1,26 +1,25 @@
 import torch
 from bound_propagation import Pow
 
-class ScalarMult(torch.nn.Linear):
+class ScalarMult(torch.nn.Linear): #\todo rename MultScalar
     def __init__(self, in_features: int, scalar: float):
         super(ScalarMult, self).__init__(in_features, in_features, bias=False)
         with torch.no_grad():
             self.weight.copy_(torch.eye(in_features) * scalar)
 
-class ScalarAdd(torch.nn.Linear):
+class ScalarAdd(torch.nn.Linear): # \todo rename AddScalar
     def __init__(self, in_features: int, scalar: float):
         super(ScalarAdd, self).__init__(in_features, in_features)
         with torch.no_grad():
             self.weight.copy_(torch.eye(in_features))
             self.bias.fill_(scalar)
 
-class SumVector(torch.nn.Linear):
+class Sum(torch.nn.Linear):
     def __init__(self, in_features: int):
-        super(SumVector, self).__init__(in_features, 1, bias=False)
+        super(Sum, self).__init__(in_features, 1, bias=False)
         with torch.no_grad():
             self.weight.fill_(1.0)
 
 class SqNorm(torch.nn.Sequential):
     def __init__(self, num_dims):
-        super().__init__(Pow(2), SumVector(num_dims))
-
+        super().__init__(Pow(2), Sum(num_dims))
