67b27596a1346890cc3694f52c3cdbb316f43b04
sjladams <steven.adams.95@hotmail.com>
2024-11-20 10:19:17 +0100

    fix bug check_mat_diag for 1d mats

--- tensors.py
+++ tensors.py
@@ -7,4 +7,6 @@ def check_mat_diag(mat: torch.Tensor) -> bool:
     """
     Check if all elements of a batch of square matrices are diagonal
     """
-    return not (mat - mat.diagonal() > PRECISION).any()
+    assert mat.shape[-1] == mat.shape[-2], "Input tensor must be a batch of square matrices"
+
+    return ((mat - torch.diag_embed(mat.diagonal(dim1=-1, dim2=-2), dim1=-1, dim2=-2)).abs() < PRECISION).all()
