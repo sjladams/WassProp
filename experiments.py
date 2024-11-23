783b22fb38f249a7bcf15e79cb9a63abbb2f8223
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2024-11-23 19:26:18 +0100

    adapt multistep

--- experiments.py
+++ experiments.py
@@ -12,15 +12,22 @@ from dynamics import Dynamics
 
 def multi_step(dynamics: Dynamics,
                loc_noise_dist: torch.Tensor,
-               covariance_noise_dist: torch.Tensor,
+               variance_noise_dist: torch.Tensor,
                loc_initial_dist: torch.Tensor,
-               covariance_initial_dist: torch.Tensor,
+               variance_initial_dist: torch.Tensor,
                num_samples: int,
                num_time_steps: int,
                num_locs: int,
-               run_type1: bool = False,
+               lr: float = 0.01,
+               num_iterations: int = 100,
                plot: bool = False,
                **kwargs):
+
+    # Get initial / noise distributions parameters
+    loc_noise_dist = torch.tensor(loc_noise_dist)
+    covariance_noise_dist = torch.diag(torch.tensor(variance_noise_dist))
+    loc_initial_dist = torch.tensor(loc_initial_dist)
+    covariance_initial_dist = torch.diag(torch.tensor(variance_initial_dist))
 
     # Initialize System Dynamics
     print(f"Global Lipschitz constant of f: {dynamics.global_lipschitz}")
@@ -41,11 +48,14 @@ def multi_step(dynamics: Dynamics,
     q0_samples = q0.sample(torch.Size((num_samples,)))
     p0_samples = q0_samples.clone()
 
-    # store wasserstein error bounds
+    # Store wasserstein error bounds
     w2_empirical = torch.zeros(num_time_steps+1)
     w2_global_lipschitz = torch.zeros(num_time_steps+1)
-    w2_type1 = torch.zeros(num_time_steps+1)
-    w2_type2 = torch.zeros(num_time_steps+1)
+    w2_lagrangian_duality = torch.zeros(num_time_steps + 1)
+
+    # Store sample trajectories
+    p_trajectories = q0_samples.clone()
+    q_trajectories = q0_samples.clone()
 
     # Propagate the system
     for k in range(num_time_steps):
@@ -79,72 +89,27 @@ def multi_step(dynamics: Dynamics,
         w2_global_lipschitz[k+1] = dynamics.global_lipschitz * (sign_q0.w2 + w2_compr + w2_global_lipschitz[k])
 
         ### Our Method
-        ## Type 1: Budget Term 2 = W_2(disc # p, disc # q)
-        if run_type1:
-            # Term 1: bound W_2(f#p_k, f#\Delta_C#p_k)
-            w2_term1_type1 = wasserstein.compute_w2_f_p__f_disc_p(
-                sign_q0, dynamics, w2_q__disc_q=sign_q0.w2 + w2_compr, w2_p__q=w2_type1[k], **kwargs)
-
-            # Term 2: bound W_2(f#\Delta#p_k, f#\Delta#q_k)
-            w2_term2_type1 = wasserstein.compute_w2_f_disc_p__f_disc_q(
-                sign_q0, dynamics, w2_q__disc_q=sign_q0.w2 + w2_compr, w2_p__q=w2_type1[k], budget_type='w2_disc_p__disc_q', **kwargs)
-
-            w2_type1[k + 1] = w2_term1_type1 + w2_term2_type1
-
-
-        ### Type 2: Budget Term 2 = W_2(p, disc # q)
-        ## Term 1: bound W_2(f#p_k, f#\Delta_C#p_k)
-        w2_term1_type2 = wasserstein.compute_w2_f_p__f_disc_p(
-            sign_q0, dynamics, w2_q__disc_q=sign_q0.w2 + w2_compr, w2_p__q=w2_type2[k], **kwargs)
-
-        ## Term 2: bound W_2(f#\Delta#p_k, f#\Delta#q_k)
-        w2_term2_type2 = wasserstein.compute_w2_f_disc_p__f_disc_q(
-            sign_q0, dynamics, w2_q__disc_q=sign_q0.w2 + w2_compr, w2_p__q=w2_type2[k], budget_type='w2_p__disc_q', **kwargs)
-
-        w2_type2[k + 1] = w2_term1_type2 + w2_term2_type2
+        w2_lagrangian_duality[k + 1] = wasserstein.compute_w2_f_p__f_disc_q_local_linear_or_constant(
+            sign_q0, dynamics, w2_q__disc_q=sign_q0.w2 + w2_compr, w2_p__q=w2_lagrangian_duality[k], lr=lr,
+            num_iterations=num_iterations)
 
         print(f"Bounds on W_2(W_2(p_{k+1}, q_{k+1})) via:\n"
-              f"\t Global Lipschits: {w2_global_lipschitz[k+1]:.4f}\n"
+              f"\t Global Lipschitz: {w2_global_lipschitz[k+1]:.4f}\n"
+              f"\t Lagrangian duality: {w2_lagrangian_duality[k+1]:.4f}\n"
               f"\t Empirical: {w2_empirical[k+1]:.4f}\n")
-        if run_type1:
-            print(f"\t Own Type 1: {w2_type1[k + 1]:.4f}\n"
-                  f"\t\t\t Term 1: {w2_term1_type1:.4f}\n"
-                  f"\t\t\t Term 2: {w2_term2_type1:.4f}\n")
-        print(f"\t Own Type 2: {w2_type2[k + 1]:.4f}\n"
-              f"\t\t\t Term 1: {w2_term1_type2:.4f}\n"
-              f"\t\t\t Term 2: {w2_term2_type2:.4f}")
-
-        if dynamics.num_dims == 1 and plot:
-            fig_propagation = plt.figure()
-            plt.hist(p0_samples.squeeze(), alpha=0.5, label=f'k={k}', bins=100, density=True)
-            plt.hist(p1_samples.squeeze(), alpha=0.5, label=f'k={k+1}', bins=100, density=True)
-            plt.legend()
-            plt.title(f"Histograms of p_{k} and p_{k+1}")
-            plt.show()
-
-            fig_signature = plt.figure()
-            plt.bar(sign_q0.locs.squeeze(), sign_q0.probs, width=0.1)
-            plt.hist(q0_samples, alpha=0.5, label=f'k={k}', bins=100, density=True)
-            plt.title(f"Signature of q_{k} and Histogram of q_{k}")
-            plt.show()
-
-            fig_approximation_error = plt.figure()
-            plt.hist(p1_samples, alpha=0.5, label=f'p_{k}', bins=100, density=True)
-            plt.hist(q1_compr_samples, alpha=0.5, label=f'q_{k}', bins=100, density=True)
-            plt.legend()
-            plt.title("Histograms q1 vs p1")
-            plt.show()
-
 
         # Overwrite for next iteration
         q0 = q1_compr
         q0_samples = q1_compr_samples
         p0_samples = p1_samples
 
+        if plot:
+            q_trajectories = torch.cat((q_trajectories, q0_samples), dim=0)
+            p_trajectories = torch.cat((p_trajectories, p0_samples), dim=0)
+
     tag = f"{dynamics.__class__.__name__} (Lipschitz={dynamics.global_lipschitz:.2f}, |C|={num_locs})"
-    w2_bounds = {'emp': w2_empirical, 'gl': w2_global_lipschitz, 'type2': w2_type2}
-    if run_type1:
-        w2_bounds['type1'] = w2_type1
+    w2_bounds = {'emp': w2_empirical, 'gl': w2_global_lipschitz, 'lagr_dual': w2_lagrangian_duality}
+
     return w2_bounds, tag
 
 
