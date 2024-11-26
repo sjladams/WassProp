79c9ea53e5392fdd842d70465d57f0ce85ef107a
sjladams <steven.adams.95@hotmail.com>
2024-11-26 19:49:55 +0100

    remove tag from single_step, generate in plotting functions

--- plot.py
+++ plot.py
@@ -44,7 +44,7 @@ def plot_multi_step(dynamics, w2_bounds, tag):
         plt.show()
 
 @torch.no_grad()
-def plot_single_step(dynamics, w2_bounds, tag, w2_p__q_options, num_locs, **kwargs):
+def plot_single_step(dynamics, w2_bounds, w2_p__q_options, num_locs, **kwargs):
 
     tag = f"{dynamics.__class__.__name__} (Lipschitz={dynamics.global_lipschitz:.2f}, |C|={num_locs})"
 
