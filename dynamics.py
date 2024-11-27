41b315b4767436a6f51666f4efb6aa21243d1677
sjladams <steven.adams.95@hotmail.com>
2024-11-27 15:45:58 +0100

    rename LinearSigmoidDynamics -> LinearDiagonalSigmoidDynamics

--- dynamics.py
+++ dynamics.py
@@ -102,14 +102,14 @@ class SigmoidDynamics(Dynamics):
     def global_lipschitz(self):
         return 0.25
 
-class LinearSigmoidDynamics(Dynamics):
+class LinearDiagonalSigmoidDynamics(Dynamics):
     def __init__(self, diagonal: Union[torch.Tensor, list], **kwargs):
         if isinstance(diagonal, list):
             diagonal = torch.tensor(diagonal)
         self.num_dims = diagonal.size(0)
         self._diagonal = diagonal
 
-        super(LinearSigmoidDynamics, self).__init__(
+        super(LinearDiagonalSigmoidDynamics, self).__init__(
             LinearDiagonalDynamics(diagonal, min=-torch.inf, max=torch.inf),
             SigmoidDynamics(self.num_dims)
         )
@@ -131,7 +131,7 @@ def get_dynamics(dynamics_type: str, **kwargs):
         return BoundedLinearDiagonalDynamics(**kwargs)
     elif dynamics_type == 'SigmoidDynamics':
         return SigmoidDynamics(**kwargs)
-    elif dynamics_type == 'LinearSigmoidDynamics':
-        return LinearSigmoidDynamics(**kwargs)
+    elif dynamics_type == 'LinearDiagonalSigmoidDynamics':
+        return LinearDiagonalSigmoidDynamics(**kwargs)
     else:
         raise ValueError(f"Unknown dynamics: {dynamics_type}")
