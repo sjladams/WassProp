ab3422a66069bcb66d295fe7adeab475c744db39
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-01-11 05:16:44 +0100

    add config

--- analysis/configs.py
+++ analysis/configs.py
@@ -3,3 +3,5 @@ FOLDER = r"C:\Users\efigueiredomot\Desktop\Papers\Wasserstein"
 NUM_LOCS_CHOICES = [10, 100, 1000]
 
 W_P__Q_CHOICES = [0.0, 0.1, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0]
+
+W_P__Q_CHOICES_OPTIMIZE_LOCS = [0.0, 0.1, 0.5, 1.0, 3.0, 5.0]
