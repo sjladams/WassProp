ad12d64290ef97d0ce27a49a4f1871d535f1dcfb
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-01-12 10:55:34 +0100

    fix configs

--- analysis/configs.py
+++ analysis/configs.py
@@ -1,6 +1,6 @@
 FOLDER = r"C:\Users\efigueiredomot\Desktop\Papers\Wasserstein"
 
-NUM_LOCS_CHOICES = [10, 100, 1000]
+NUM_LOCS_CHOICES = [10, 100, 1000, 10000]
 
 W_P__Q_CHOICES = [0.0, 0.1, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0]
 
