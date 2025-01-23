920e18003b8446f425088f672412b6178e395d3a
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-01-23 20:43:09 +0100

    add auxiliary methods for discrete distributions

---
+++ utils_distributions.py
@@ -0,0 +1,24 @@
+import torch
+
+def cross_product(state_signature, noise_signature):
+
+    n, m = state_signature.locs.size(0), noise_signature.locs.size(0)
+    d, q = state_signature.locs.shape[-1], noise_signature.locs.shape[-1]
+
+    cross_locs = torch.cat((
+        state_signature.locs.unsqueeze(1).expand(-1, m, -1),
+        noise_signature.locs.unsqueeze(0).expand(n, -1, -1)),
+        dim=-1).view(-1, d + q)
+
+    cross_probs = ( state_signature.probs.unsqueeze(1) * noise_signature.probs.unsqueeze(0) ).view(-1)
+
+    return cross_probs, cross_locs
+
+def sum_discrete_distributions(state_signature, noise_signature):
+
+    d = state_signature.locs.size(-1)
+
+    sum_locs = ( state_signature.locs.unsqueeze(1) + noise_signature.locs.unsqueeze(0) ).view(-1, d)
+    sum_probs = ( state_signature.probs.unsqueeze(1) * noise_signature.probs.unsqueeze(0) ).view(-1)
+
+    return sum_probs, sum_locs
