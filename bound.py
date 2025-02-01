76b692e456adf426a5ca25b70a70418233bf711e
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-01 02:26:37 +0100

    remove projection methods, comment out tests, remove gd for alpha

--- bound.py
+++ bound.py
@@ -10,66 +10,12 @@ from tensors import check_mat_diag
 
 factory = bp.BoundModelFactory()
 
-def get_proj_matrix(vp: HyperRectangularVoronoiPartition):
-    """
-    Compute proj_{R_k}(c_i) for all signature locations c_i and regions R_k in the voronoi partition, and store
-    in matrix with i-th row corresponding to c_i and k-th column corresponding to R_k.
-
-    CONVENTION: indexing regions over columns and centers over rows
-
-    :param vp: VornoiPartition
-    """
-    locs_ex = vp.locs.unsqueeze(-2)
-    l_ex = vp.lower.unsqueeze(-3)
-    u_ex = vp.upper.unsqueeze(-3)
-
-    # Compute the projections for all locs and regions that do not overlap, set overlapping to nan
-    mask_c_smaller_l = locs_ex <= l_ex
-    mask_c_larger_u = locs_ex >= u_ex
-    mask_c_in_region = torch.logical_and(~mask_c_larger_u, ~mask_c_smaller_l).all(-1)
-
-    below_l = torch.where(mask_c_smaller_l, l_ex, torch.zeros_like(l_ex))
-    above_u = torch.where(mask_c_larger_u, u_ex, torch.zeros_like(l_ex))
-    overlapping = torch.where(mask_c_in_region.unsqueeze(-1).repeat(1,1,vp.locs.size(-1)),
-                              torch.zeros_like(locs_ex).fill_(torch.nan),
-                              torch.zeros_like(locs_ex))
-
-    # Calculate the projection, summing both below and above cases
-    proj_matrix = below_l + above_u + overlapping
-
-    # Account for proj_{R_i}(c_i) = c_i
-    proj_matrix.diagonal(dim1=-3, dim2=-2).copy_(vp.locs.swapaxes(-1, -2))
-
-    # Handle non-overlapping regions:
-    if vp.shell[:,0].isneginf().all() and vp.shell[:,1].isinf().all():
-        proj_matrix[:, -1] = vp.locs  # To guarantee numerical stability, we set the projection on an empty set to zero
-    else:
-        closest_edge_shell = torch.where(
-            (vp.locs - vp.shell[..., 0]).abs() < (vp.locs - vp.shell[..., 1]).abs(),
-            vp.shell[..., 0],
-            vp.shell[..., 1]
-        )
-        proj_matrix[:-1, -1] = closest_edge_shell[:-1]
-
-    return torch.nan_to_num(proj_matrix)
-
-def get_norm_of_proj_matrix(vp: HyperRectangularVoronoiPartition):
-    """
-    Compute ||proj_{R_k}(c_i) - c_i||_2 for all signature locations c_i and regions R_k in the voronoi
-    partition, and store in matrix with i-th row corresponding to c_i and k-th column corresponding to R_k.
-
-    :param vp: VoronoiPartition
-    """
-    proj_matrix = get_proj_matrix(vp)
-    return torch.norm(proj_matrix - vp.locs.unsqueeze(-2), dim=-1, p=2)
-
-
 class SqNormFxSubFz(torch.nn.Sequential):
     def __init__(self, f):
         super().__init__(
             bp.Parallel(f, f, split_size=f.num_dims),
             bp.VectorSub(),
-            SqNorm(f.num_dims if isinstance(f, AdditiveGaussianDynamics) else f.num_state_dims)
+            SqNorm(f.num_dims)
         )
 
 
@@ -143,8 +89,8 @@ def _global_lbp_sq_norm_fx_fc_quadrant(
     input_bound = bp.HyperRectangle(lower, upper)
     lb = linear_factory.build(f).crown_ibp(input_bound)
 
-    assert independent_dims or check_mat_diag(lb.lower[0]) and check_mat_diag(lb.upper[0]), \
-        "Currently global_lbp_sq_norm_fc only works for independent dimensions"
+    #assert independent_dims or check_mat_diag(lb.lower[0]) and check_mat_diag(lb.upper[0]), \
+    #    "Currently global_lbp_sq_norm_fc only works for independent dimensions"
 
     # From linear bounds to bounds on the norms:
     alpha = torch.max(
@@ -210,27 +156,5 @@ def global_lbp_sq_norm_fx_fc(
             alphas[idx] = _global_lbp_sq_norm_fx_fc_quadrant(f, locs, quadrant[0], quadrant[1], independent_dims )
 
         alpha = alphas.max(dim=0).values.clamp(min=0., max=f.global_lipschitz**2)
-    else:
-        # below we use a non-formal optimization based method. Using the bound-propagation package result in very-
-        # conservative results
-
-        def compute_local_lipschitz(x):
-            local_lipschitz = ((f(x) - f(locs)).pow(2).sum(-1) - beta) / (x - locs).pow(2).sum(-1)
-            local_lipschitz = torch.nan_to_num(local_lipschitz, nan=f.global_lipschitz ** 2)
-            return local_lipschitz
-
-        def objective(x):
-            return - compute_local_lipschitz(x).sum() # or take mean?
-
-        x_opt, losses = minimize_with_adam(
-            objective,
-            param=(locs.clone().detach() + torch.randn_like(locs)).requires_grad_(True),
-            lr=0.01,
-            num_iterations=5000,
-            tolerance=1e-8,
-            print_progress=True
-        )
-
-        alpha = compute_local_lipschitz(x_opt).detach().clamp(min=0., max=f.global_lipschitz**2)
-
+        
     return alpha
