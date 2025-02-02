1c167c0bc47c0cd2cadd463eb8fa6746ea10c0cf
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-02 04:28:45 +0100

    add quantization based on given locs

--- utils_distributions.py
+++ utils_distributions.py
@@ -1,6 +1,10 @@
 import torch
 import discretize_distributions as ds
 
+import wasserstein
+from regions import HyperRectangularVoronoiPartition
+
+
 def cross_product(state_signature, noise_signature):
 
     n, m = state_signature.locs.size(0), noise_signature.locs.size(0)
@@ -24,17 +28,30 @@ def sum_discrete_distributions(state_signature, noise_signature):
 
     return sum_probs, sum_locs
 
-def quantize(q, num_locs):
+def quantize(q, num_locs, locs = None):
 
-    if isinstance(q, ds.MultivariateNormal):
-        sign_q = ds.discretization_generator(dist=q, num_locs=num_locs)
-        w2_quantization = sign_q.w2
+    if locs is None:
+        if isinstance(q, ds.MultivariateNormal):
+            sign_q = ds.discretization_generator(dist=q, num_locs=num_locs)
+            w2_quantization = sign_q.w2
 
-    elif isinstance(q, ds.DiscretizedMultivariateNormal) or isinstance(q, ds.CategoricalFloat):
-        sign_q = q
-        w2_quantization = 0.0
+        elif isinstance(q, ds.DiscretizedMultivariateNormal) or isinstance(q, ds.CategoricalFloat):
+            sign_q = q
+            w2_quantization = 0.0
 
-    else:
-        raise ValueError('Quantization not implemented for q class.')
+        else:
+            raise ValueError('Optimal quantization not implemented for q class.')
+
+    else: # Quantize given locs using Voronoi partitions
+        if isinstance(q, ds.MultivariateNormal):
+            voronoi_partition = HyperRectangularVoronoiPartition(locs)
+            compute_sq_norm_2nd_moment = wasserstein.compute_sq_norm_2nd_moment(q, voronoi_partition, locs)
+
+            raise ValueError('NEED TO IMPLEMENT P(R_i) FOR GAUSSIAN.')
+
+            #return torch.einsum('...i,...i->...', compute_sq_norm_2nd_moment, probs)
+
+        else:
+            raise ValueError('Quantization given locs not implemented for q class.')
 
     return sign_q, w2_quantization
