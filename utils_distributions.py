88de4ba63265ddfa8f79fac357bb46831a5c2272
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-08 13:38:15 +0100

    revert changes in quantize method

--- utils_distributions.py
+++ utils_distributions.py
@@ -28,30 +28,17 @@ def sum_discrete_distributions(state_signature, noise_signature):
 
     return sum_probs, sum_locs
 
-def quantize(q, num_locs, locs = None):
+def quantize(q, num_locs):
 
-    if locs is None:
-        if isinstance(q, ds.MultivariateNormal):
-            sign_q = ds.discretization_generator(dist=q, num_locs=num_locs)
-            w2_quantization = sign_q.w2
+    if isinstance(q, ds.MultivariateNormal):
+        sign_q = ds.discretization_generator(dist=q, num_locs=num_locs)
+        w2_quantization = sign_q.w2
 
-        elif isinstance(q, ds.DiscretizedMultivariateNormal) or isinstance(q, ds.CategoricalFloat):
-            sign_q = q
-            w2_quantization = 0.0
+    elif isinstance(q, ds.DiscretizedMultivariateNormal) or isinstance(q, ds.CategoricalFloat):
+        sign_q = q
+        w2_quantization = 0.0
 
-        else:
-            raise ValueError('Optimal quantization not implemented for q class.')
-
-    else: # Quantize given locs using Voronoi partitions
-        if isinstance(q, ds.MultivariateNormal):
-            voronoi_partition = HyperRectangularVoronoiPartition(locs)
-            compute_sq_norm_2nd_moment = wasserstein.compute_sq_norm_2nd_moment(q, voronoi_partition, locs)
-
-            raise ValueError('NEED TO IMPLEMENT P(R_i) FOR GAUSSIAN.')
-
-            #return torch.einsum('...i,...i->...', compute_sq_norm_2nd_moment, probs)
-
-        else:
-            raise ValueError('Quantization given locs not implemented for q class.')
+    else:
+        raise ValueError('Optimal quantization not implemented for q class.')
 
     return sign_q, w2_quantization
