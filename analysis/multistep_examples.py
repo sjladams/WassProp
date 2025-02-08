1dee7f2517c257bc1403d76d7b7efbb7048cca6a
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-08 19:33:32 +0100

    add multistep analysis

---
+++ analysis/multistep_examples.py
@@ -0,0 +1,44 @@
+import torch
+from experiments import multi_step, get_noise_dist, get_initial_dist, single_step_w2_options
+from dynamics import get_dynamics
+import plot
+from utils import load_params, parse_arguments
+
+def multistep_approximation(dynamics_type, num_dims, dyn_setting, num_locs):
+    args = parse_arguments(
+        dynamics_type=dynamics_type,
+        num_dims=num_dims,
+        dynamics_setting=dyn_setting,
+        num_locs=num_locs,
+        num_locs_after_compr=num_locs,
+        num_samples=5000,
+        optimize_locs=False
+    )
+
+    params = load_params(args)
+
+    dynamics = get_dynamics(**params)
+    initial_dist = get_initial_dist(**params)
+    noise_dist = get_noise_dist(**params)
+
+
+    w2_bounds, samples = multi_step(
+        dynamics=dynamics,
+        noise_dist=noise_dist,
+        q=initial_dist,
+        num_time_steps=10,
+        run_lagrangian_duality=True,
+        run_empirical=True,
+        **params
+    )
+    plot.plot_multi_step(dynamics, samples, type=dynamics_type)
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    dynamics_type = 'LinearDynamics'
+    num_dims = 4
+    num_locs = 500
+    dyn_setting = 0
+
+    multistep_approximation(dynamics_type, num_dims, dyn_setting, num_locs)
