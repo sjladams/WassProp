e213a7311cd4774ae1dce0e928006df9a5f38978
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-09 19:25:25 +0100

    simplify code for sigmoid example

--- analysis/sigmoid_example.py
+++ analysis/sigmoid_example.py
@@ -1,4 +1,5 @@
 import torch
+import wasserstein
 from experiments import multi_step, get_noise_dist, get_initial_dist, single_step_w2_options
 from dynamics import get_dynamics
 import plot
@@ -11,36 +12,24 @@ def initial_step(dynamics_type, num_dims, dyn_setting, nums_locs):
     signatures, bounds = [], []
 
     for num_locs in nums_locs:
+
         args = parse_arguments(
             dynamics_type=dynamics_type,
             num_dims=num_dims,
             dynamics_setting=dyn_setting,
             num_locs=num_locs,
             num_locs_after_compr=num_locs,
-            num_samples=5000,
-            optimize_locs=False
         )
-
         params = load_params(args)
 
         dynamics = get_dynamics(**params)
         initial_dist = get_initial_dist(**params)
-        noise_dist = get_noise_dist(**params)
 
         signature, w2 = quantize(initial_dist, num_locs)
         signatures.append(signature)
 
-        w2_bounds = single_step_w2_options(
-            dynamics=dynamics,
-            noise_dist=noise_dist,
-            q=initial_dist,
-            w2_p__q_options=[0.],
-            run_independent_coupling=False,
-            run_lagrangian_duality=True,
-            **params
-        )
-
-        bound = w2_bounds[0.0]['lagrangian_duality'].item()
+        fn_sq_w2_f_q__f_disc_q = wasserstein.get_fn_sq_w2_f_q__f_disc_q(signature, dynamics)
+        bound = fn_sq_w2_f_q__f_disc_q().sqrt()
         bounds.append(bound)
 
     plot.plot_signatures(dynamics, initial_dist, signatures, bounds)
