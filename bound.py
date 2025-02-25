d3b4471ed933f1c05917d6d8c00d02a2d120420e
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-02-25 14:08:02 +0100

    solve for lack of field 'num_state_dims'

--- bound.py
+++ bound.py
@@ -69,7 +69,7 @@ class SqNormFxSubFz(torch.nn.Sequential):
         super().__init__(
             bp.Parallel(f, f, split_size=f.num_dims),
             bp.VectorSub(),
-            SqNorm(f.num_dims if isinstance(f, AdditiveGaussianDynamics) else f.num_state_dims)
+            SqNorm(f.num_dims)
         )
 
 
