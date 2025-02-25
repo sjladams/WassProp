9cde004ec6a129d2109822c78126195ba7020a17
sjladams <steven.adams.95@hotmail.com>
2025-02-25 16:32:01 +0100

    Make Arithmetic modules inherit the Linear module from linear_bound_propagation

--- linear_bound_propagation/arithmetic.py
+++ linear_bound_propagation/arithmetic.py
@@ -1,26 +1,20 @@
 import torch
 import bound_propagation as bp
+from .linear import Linear
 
 __all__ = ['ScalarMult', 'ScalarAdd', 'Sum', 'SqNorm']
 
-class ScalarMult(torch.nn.Linear): #\todo rename MultScalar
+class ScalarMult(Linear): #\todo rename MultScalar
     def __init__(self, in_features: int, scalar: float):
-        super(ScalarMult, self).__init__(in_features, in_features, bias=False)
-        with torch.no_grad():
-            self.weight.copy_(torch.eye(in_features) * scalar)
+        super(ScalarMult, self).__init__(torch.eye(in_features) * scalar)
 
-class ScalarAdd(torch.nn.Linear): # \todo rename AddScalar
+class ScalarAdd(Linear): # \todo rename AddScalar
     def __init__(self, in_features: int, scalar: float):
-        super(ScalarAdd, self).__init__(in_features, in_features)
-        with torch.no_grad():
-            self.weight.copy_(torch.eye(in_features))
-            self.bias.fill_(scalar)
+        super(ScalarAdd, self).__init__(torch.eye(in_features), torch.as_tensor(scalar))
 
-class Sum(torch.nn.Linear):
+class Sum(Linear):
     def __init__(self, in_features: int):
-        super(Sum, self).__init__(in_features, 1, bias=False)
-        with torch.no_grad():
-            self.weight.fill_(1.0)
+        super(Sum, self).__init__(torch.ones(1, in_features))
 
 class SqNorm(torch.nn.Sequential):
     def __init__(self, num_dims):
