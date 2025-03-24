65ac45c621916e7bb4df204da79af2ec1221d109
sjladams <steven.adams.95@hotmail.com>
2025-03-24 12:48:57 +0100

    Sync scripts in paper_experiments with package updates. Not that compare_quantization_operator.py is disabled

--- paper_experiments/stochastic_dynamical_systems.py
+++ paper_experiments/stochastic_dynamical_systems.py
@@ -3,24 +3,22 @@ from experiments import multi_step
 from utils_distributions import get_noise_dist, get_initial_dist
 from dynamics import get_dynamics
 import plot
-from utils import load_params, parse_arguments
+from utils import parse_arguments
 
-def multistep_approximation(dynamics_type, num_dims, dyn_setting, num_locs):
+def multistep_approximation(dynamics_type, dyn_setting, num_locs):
     args = parse_arguments(
         dynamics_type=dynamics_type,
-        num_dims=num_dims,
         dynamics_setting=dyn_setting,
         num_locs=num_locs,
         num_locs_after_compr=num_locs,
         num_samples=5000
     )
-    params = load_params(args)
 
-    dynamics = get_dynamics(**params)
-    initial_dist = get_initial_dist(**params)
-    noise_dist = get_noise_dist(**params)
+    dynamics = get_dynamics(**vars(args))
+    initial_dist = get_initial_dist(args.loc_initial_dist, args.variance_initial_dist)
+    noise_dist = get_noise_dist(args.loc_noise_dist, args.variance_noise_dist)
 
-    w2_bounds, samples = multi_step(
+    _, _, samples = multi_step(
         dynamics=dynamics,
         noise_dist=noise_dist,
         q=initial_dist,
@@ -28,7 +26,8 @@ def multistep_approximation(dynamics_type, num_dims, dyn_setting, num_locs):
         run_lagrangian_duality=True,
         run_empirical=True,
         propagate_via_gmm=False,
-        **params
+        num_samples=args.num_samples,
+        num_locs=args.num_locs
     )
     plot.plot_multi_step(dynamics, samples, type=dynamics_type)
 
@@ -36,8 +35,7 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     dynamics_type = 'MountainCarDynamics'
-    num_dims = 2
     num_locs = 100
     dyn_setting = 0
 
-    multistep_approximation(dynamics_type, num_dims, dyn_setting, num_locs)
+    multistep_approximation(dynamics_type, dyn_setting, num_locs)
