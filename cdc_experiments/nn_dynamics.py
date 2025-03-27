3c75bf05e886695693e7134d4e92bca1e63bf957
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-03-28 00:45:21 +0100

    change variance of distributions, increase size

--- cdc_experiments/nn_dynamics.py
+++ cdc_experiments/nn_dynamics.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         dynamics_type = "NeuralPendulumDynamics",
         dynamics_setting = 0,
         num_locs = 100,
-        size_after_compr=5,
+        size_after_compr=10,
         num_samples = 100,
         lr = 0.01,
         num_iterations = 100,
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     noise_dist = get_noise_dist(args.loc_noise_dist, args.variance_noise_dist)
 
     w2_q__sign_q_store, w2_p1__q1_store, samples_store, q_store = multi_step(
-        w2_p__q= 0.01,
+        w2_p__q= 0.001,
         w2_noise_dist= 0.001,
         dynamics=dynamics,
         noise_dist=noise_dist,
