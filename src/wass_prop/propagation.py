7de751807e876642cf7441b85b270434ae9d5a24
sjladams <steven.adams.95@hotmail.com>
2026-08-18 08:56:08 +0200

    add option use_additive_noise

--- src/wass_prop/propagation.py
+++ src/wass_prop/propagation.py
@@ -17,8 +17,9 @@ def single_step(
     noise: AmbiguityBall,
     num_locs: int,
     use_lagrangian_duality: bool = True,
+    use_additive_noise: bool = True,
 ) -> AmbiguityBall:
-    if isinstance(dynamics, AdditiveNoiseDynamics):
+    if isinstance(dynamics, AdditiveNoiseDynamics) and use_additive_noise:
         return _single_step_additive_noise(dynamics, q, noise, num_locs, use_lagrangian_duality)
     else:
         return _single_step_general_noise(dynamics, q, noise, num_locs, use_lagrangian_duality)
@@ -33,26 +34,51 @@ def _single_step_general_noise(
     disc_q, w2_q__disc_q = discretize(q, num_locs)
     disc_noise_dist, w2_noise_dist__disc_noise_dist = discretize(noise, num_locs)
 
-    q1 = propagate_general_discrete_noise(dynamics, disc_noise_dist, disc_q)
-
-    if use_lagrangian_duality:
-        w2_p__q = q.w2 + noise.w2
-        w2_q__disc_q = w2_q__disc_q + w2_noise_dist__disc_noise_dist
-
-        if isinstance(q.center, dd_dists.MultivariateNormal) and w2_p__q == 0.:
-            w2_p1__q1 = wasserstein.compute_w2_f_q__f_disc_q_lagrangian_duality(
-                q=q.center,
-                disc_q=disc_q,
-                f=dynamics,
-            )
-        else:
-            w2_p1__q1 = wasserstein.compute_w2_f_p__f_disc_q_lagrangian_duality(
-                disc_q=disc_q,
-                f=dynamics,
-                w2_p__disc_q=w2_p__q + w2_q__disc_q
-            )
+    if isinstance(dynamics, AdditiveNoiseDynamics):
+        q1 = propagate_additive_noise(dynamics, disc_noise_dist, disc_q)
     else:
-        w2_p1__q1 = dynamics.global_lipschitz * (w2_q__disc_q + q.w2 + w2_noise_dist__disc_noise_dist + noise.w2)
+        q1 = propagate_general_discrete_noise(dynamics, disc_noise_dist, disc_q)
+
+    if isinstance(dynamics, AdditiveNoiseDynamics):
+        if use_lagrangian_duality:
+            w2_p__q = q.w2
+
+            if isinstance(q.center, dd_dists.MultivariateNormal) and w2_p__q == 0.:
+                w2_p1__q1 = wasserstein.compute_w2_f_q__f_disc_q_lagrangian_duality(
+                    q=q.center,
+                    disc_q=disc_q,
+                    f=dynamics.state_dynamics,
+                )
+            else:
+                w2_p1__q1 = wasserstein.compute_w2_f_p__f_disc_q_lagrangian_duality(
+                    disc_q=disc_q,
+                    f=dynamics.state_dynamics,
+                    w2_p__disc_q=w2_p__q + w2_q__disc_q,
+                )
+
+            w2_p__q += w2_noise_dist__disc_noise_dist + noise.w2
+        else:
+            w2_p1__q1 = dynamics.global_lipschitz * (w2_q__disc_q + q.w2) + w2_noise_dist__disc_noise_dist + noise.w2
+
+    else:
+        if use_lagrangian_duality:
+            w2_p__q = q.w2 + noise.w2
+            w2_q__disc_q = w2_q__disc_q + w2_noise_dist__disc_noise_dist
+
+            if isinstance(q.center, dd_dists.MultivariateNormal) and w2_p__q == 0.:
+                w2_p1__q1 = wasserstein.compute_w2_f_q__f_disc_q_lagrangian_duality(
+                    q=q.center,
+                    disc_q=disc_q,
+                    f=dynamics,
+                )
+            else:
+                w2_p1__q1 = wasserstein.compute_w2_f_p__f_disc_q_lagrangian_duality(
+                    disc_q=disc_q,
+                    f=dynamics,
+                    w2_p__disc_q=w2_p__q + w2_q__disc_q
+                )
+        else:
+            w2_p1__q1 = dynamics.global_lipschitz * (w2_q__disc_q + q.w2 + w2_noise_dist__disc_noise_dist + noise.w2)
 
     return AmbiguityBall(center=q1, radius=w2_p1__q1)
 
@@ -65,7 +91,7 @@ def _single_step_additive_noise(
 ) -> AmbiguityBall:
     disc_q, w2_q__disc_q = discretize(q, num_locs)
 
-    q1 = propagate_additive_gaussian_noise(dynamics, noise.center, disc_q)
+    q1 = propagate_additive_noise(dynamics, noise.center, disc_q)
 
     if use_lagrangian_duality:
         w2_p__q = q.w2
@@ -110,6 +136,7 @@ def multi_step(
     num_time_steps: int,
     num_locs: int,
     use_lagrangian_duality: bool = True,
+    use_additive_noise: bool = True,
     print_progress: bool = True,
 ) -> Path:
 
@@ -122,7 +149,8 @@ def multi_step(
             q=path.at(k-1),
             noise=noise,
             num_locs=num_locs,
-            use_lagrangian_duality=use_lagrangian_duality
+            use_lagrangian_duality=use_lagrangian_duality, 
+            use_additive_noise=use_additive_noise,
         ))
         if print_progress:
             print(f"W_2(p_{k+1}, q_{k+1}) <= {path.at(k).w2:.4f}:\n")
@@ -180,7 +208,7 @@ def multi_step_empirical(
     return path
 
 
-def propagate_additive_gaussian_noise(
+def propagate_additive_noise(
         dynamics: AdditiveNoiseDynamics,
         noise_dist: Union[dd_dists.MultivariateNormal, dd_dists.MixtureMultivariateNormal, dd_dists.CategoricalFloat],
         disc_state_dist: dd_dists.CategoricalFloat
